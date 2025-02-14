fbb79adb50d7b2b4cc00dd4c2a2d09b21f5b65c1
MFactor1 <mingsqu@gmail.com>
2025-02-14 02:17:05 -0500

    Update frontend using motion

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link, useNavigate, useLocation } from "react-router-dom";
+import { motion, animate, AnimatePresence } from 'motion/react';
 
 import Home from './pages/home'
 import RecipeViewer from './pages/recipeViewer'
@@ -22,15 +23,45 @@ function App() {
 }
 
 function Header() {
+  const tabs = [
+    {path: "/", label: "Home"},
+    {path: "/recipe-creator", label: "Recipe Creator"},
+    {path: "/recipe-viewer", label: "Recipe Viewer"},
+  ];
   const navigator = useNavigate();
   return (
-    <header className="headerbar">
-      <h1>Eaty</h1>
-      <nav className="navbar" style={{ display: "flex", flexDirection: "row", textAlign:"left"}}>
-        <button onClick={() => {navigator('/')}} style={{ marginRight: "20px" }}>Home</button>
-        <button onClick={() => {navigator('recipe-creator')}} style={{ marginRight: "20px" }}>Recipe Creator</button>
-      </nav>
-    </header>
+    <>
+      <header className="headerbar">
+        <h1 className="title">Eat<span className='titleHighlight'>y</span></h1>
+        <nav className="navbar">
+          {tabs.map((item) => (
+            <motion.li
+              key={item.path}
+              initial={false}
+              className='navTab'
+              onClick={() => navigator(item.path)}
+            >
+              {item.label}
+              {item.path == useLocation().pathname ? (
+                <AnimatePresence>
+                  <motion.div
+                    className='navUnderline'
+                    layoutId='navUnderline'
+                    id='navUnderline'
+                    initial={{
+                      //scale: 0.5,
+                    }}
+                    animate={{
+                      scale: 0.5,
+                    }}
+                  />
+                </AnimatePresence>
+              ) : null}
+            </motion.li>
+          ))}
+        </nav>
+      </header>
+    </>
   );
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import { AnimatePresence, motion } from 'motion/react';
+import { useState, useEffect } from 'react';
+
+const items = [0, 1, 2, 3];
 
 export default function Home() {
+  const [reRoll, setReRoll] = useState(false);
+  const [rollVisible, setRollVisible] = useState(false);
+  const [resultVisible, setResultVisible] = useState(false);
+
+  useEffect(() => {
+    if (reRoll) {
+      let extra_delay = 1
+      if (!resultVisible) {
+        extra_delay = 0;
+      }
+      setResultVisible(false);
+      setTimeout(() => {
+        setRollVisible(true);
+      }, 600 * extra_delay);
+      setTimeout(() => {
+        setRollVisible(false);
+      }, 800 + 600 * extra_delay);
+      setTimeout(() => {
+        setReRoll(false);
+        setResultVisible(true);
+      }, 1700 + 600 * extra_delay);
+    }
+  }, [reRoll]);
+
   return (
-    <div>
-      <p>
-        Home Page
-      </p>
+    <div style={{ marginTop: "30px" }}>
+      <motion.button
+        className='rollBtn'
+        onClick={() => reRoll ? null : setReRoll(true)}
+        whileTap={{ y: 1 }}
+      >
+        {!reRoll && !resultVisible ? "Roll" : "Re-Roll"}
+      </motion.button>
+      <ul className = 'rollDisplay'>
+        <AnimatePresence>
+          {items.map((index) => (
+            rollVisible ?
+              <motion.li
+                key={index}
+                className='list-item'
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -50 }}
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+              />
+            : null
+          ))}
+          {resultVisible ? <motion.p
+            className='rollResult'
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -50 }}
+            transition={{ duration: 0.5 }}
+          >
+            Tuscan Shrimp Marinara
+          </motion.p> : null}
+        </AnimatePresence>
+      </ul>
     </div>
   );
 }
